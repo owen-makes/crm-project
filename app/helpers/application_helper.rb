b1399768e4017143356319fc557c9955b7ef6433
owen-makes <owen.botto@gmail.com>
2025-01-27 23:23:16 -0300

    Fixed some tailwind error in banner using a helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def notice_banner(name)
+    if name == "notice"
+      "bg-green-100 border-green-400 text-green-700"
+    else
+      "bg-red-100 border-red-400 text-red-700"
+    end
+  end
 end
