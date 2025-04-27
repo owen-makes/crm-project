e9cac9acf528dc78359680204bba35a32d36bf90
owen-makes <owen.botto@gmail.com>
2025-04-27 18:09:21 -0300

    Improve AddButton viewcomponent

--- app/components/addbutton_component.rb
+++ app/components/addbutton_component.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 class AddbuttonComponent < ViewComponent::Base
-  attr_reader :label, :link
+  attr_reader :label, :link, :data, :classes
 
-  def initialize(label: "Nuevo", link:, turbo_frame: nil)
-    @label = label
-    @link = link
-    @turbo_frame = turbo_frame  # Pass the name of the turbo frame
+  DEFAULT_CLASSES =
+    "inline-flex items-center px-3 py-1.5 border border-black rounded-md
+     font-medium text-xs hover:bg-slate-50 focus:outline-none
+     focus:ring-2 focus:ring-offset-2 focus:ring-green-500".squish.freeze
+
+  def initialize(label: "Nuevo", link:, data: {}, classes: DEFAULT_CLASSES)
+    @label   = label
+    @link    = link
+    @data    = data.compact_blank
+    @classes = classes
   end
 end
