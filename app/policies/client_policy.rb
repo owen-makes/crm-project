1f040c4907ff4c6fa55c42d873b867b96feaa5db
owen-makes <owen.botto@gmail.com>
2025-05-01 13:19:45 -0300

    CRITICAL: Fix Client/Lead policy bug that would let an admin user without a team see all indep. advisors clients/leads

--- app/policies/client_policy.rb
+++ app/policies/client_policy.rb
@@ -1,35 +1,47 @@
 class ClientPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      if user.admin?
+      if admin_with_team?
         scope.joins(:user).where(users: { team_id: user.team_id })
       else
         scope.where(user: user)
       end
     end
+
+    private
+
+    def admin_with_team?
+      user.admin? && user.team_id.present?
+    end
   end
-  # scope: Represents the collection of resources Lead.all
-  # resolve: Defines how the collection should be filtered based on the user's role.
+
+  # ---- PERMISSIONS ---------------------------------------------------------
 
   def show?
-    # Members can only see their leads; admins can see any lead in their team
-    user.admin? ? record.user.team == user.team : record.user == user
+    admin_with_team? ? same_team? : owns_record?
   end
 
   def assign?
-    # Admin can assign if the lead is in their team
-    user.admin? && record.user.team == user.team
+    admin_with_team? && same_team?
   end
 
-  def edit?
-    show?
+  alias_method :edit?,    :show?
+  alias_method :update?,  :show?
+  alias_method :destroy?, :show?
+
+  # ---- HELPERS -------------------------------------------------------------
+
+  private
+
+  def admin_with_team?
+    user.admin? && user.team_id.present?
   end
 
-  def update?
-    show?
+  def same_team?
+    record.user&.team_id == user.team_id
   end
 
-  def destroy?
-    show?
+  def owns_record?
+    record.user_id == user.id
   end
 end
